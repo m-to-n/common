1fa187ada3a95985a2d07de14f0f19530f690980
Adam Bezecny <adam.bezecny@gmail.com>
2022-03-08 19:37:24 +0100

    tenant structs enabled for json ser/deser

--- tenants/tenants.go
+++ tenants/tenants.go
@@ -7,25 +7,25 @@ import (
 // use extension interface pattern to replace inheritance with composition
 // see https://medium.com/swlh/what-is-the-extension-interface-pattern-in-golang-ce852dcecaec
 type TenantChannelConfig struct {
-	Channel channels.ChannelType
+	Channel channels.ChannelType `json:"channel"`
 }
 
 type ChannelConfigWhatsAppNumbers struct {
-	PhoneNumber string
-	Language    string
+	PhoneNumber string `json:"phoneNumber"`
+	Language    string `json:"language"`
 }
 
 type TenantChannelConfigWhatsApp struct {
 	TenantChannelConfig
-	AccountSid string
-	Numbers    []ChannelConfigWhatsAppNumbers
+	AccountSid string                         `json:"accountSid"`
+	Numbers    []ChannelConfigWhatsAppNumbers `json:"numbers"`
 }
 
 // represents configuration of platform tenant
 // for now this means tenant channels, later NLPs, human providers, etc.
 type TenantConfig struct {
-	TenantId string
-	Name     string
-	Desc     string
-	Channels []TenantChannelConfig
+	TenantId string                `json:"tenantId"`
+	Name     string                `json:"name"`
+	Desc     string                `json:"desc"`
+	Channels []TenantChannelConfig `json:"channels"`
 }
